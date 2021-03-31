86b34d55bc33992d5b7e435830029da6183768e9
ramtarun <ramtarun.b2013@gmail.com>
2021-03-31 17:56:55 -0400

    login and registration endpoints

--- services/roomService/src/router/auth.ts
+++ services/roomService/src/router/auth.ts
@@ -1,72 +1,75 @@
-import Router from 'express';
-import { json } from 'body-parser';
-import cors from 'cors';
-// import { OAuth2Client } from 'google-auth-library';
-import { sign, verify, JsonWebTokenError } from 'jsonwebtoken';
-import { config } from 'dotenv';
+import { Express } from 'express';
+import { Server } from 'http';
+import { StatusCodes } from 'http-status-codes';
+import BodyParser, { json } from 'body-parser';
 
-/* Refer to this link to understand how json web tokens validate the tokens
-    https://www.sohamkamani.com/blog/javascript/2019-03-29-node-jwt-authentication/
+import { logError } from '../Utils';
+import {
+  userRegistrationRequestHandler,
+  userLoginRequestHandler,
+  userLogoutRequestHandler,
+} from '../requestHandlers/UserAuthRequestHandler';
 
-    Refer to this link to understand how routing middleware can be used to authenticate
-    https://scotch.io/tutorials/route-middleware-to-check-if-a-user-is-authenticated-in-node-js
-*/
-
-config();
-
-const routes = Router();
-
-const { JWT_SECRET } = process.env;
-
-const uiServerOrigin = process.env.UI_SERVER_ORIGIN || 'http://localhost:3000';
-
-routes.use(cors({ origin: uiServerOrigin, credentials: true }));
-routes.use(json());
-
-routes.post('/signin', async (req, res) => {
-  //   const client = new OAuth2Client();
-  //   const ticket = await client.verifyIdToken({ idToken: req.body.google_tokenId }).catch((e) => res.send(`Invalid Credentials: ${e}`));
-  //   const tokenData = ticket.getPayload();
-  //   const { given_name: givenName, name, email } = tokenData;
-  //   const credentials = {
-  //     signedIn: true,
-  //     givenName,
-  //     name,
-  //     email,
-  //   };
-  //   const token = sign(credentials, JWT_SECRET);
-  //   /* Uncomment below line so that it works on your localhost and comment the line next to it */
-  //   res.cookie('jwt', token, { httpOnly: true });
-  //   /* This following line is critical for production phase, uncomment this line while deploying */
-  //   // res.cookie('jwt', token, { httpOnly: true, sameSite: 'None', secure: true }); // Critical line needed in production phase
-  //   res.json(credentials);
-});
-
-routes.post('/signout', async (req, res) => {
-  res.clearCookie('jwt');
-  res.json({ status: 'ok' });
-});
-
-const validateAPIRequest = (req, res) => {
-  const validatetoken = req.cookies.jwt;
-  try {
-    const validatedCredentials = verify(validatetoken, JWT_SECRET as string);
-    console.log(
-      'validated credentials..........................................................................',
-    );
-    return validatedCredentials;
-  } catch (e) {
-    if (e instanceof JsonWebTokenError) {
-      // if the error thrown is because the JWT is unauthorized, return a 401 error
-      return res
-        .status(401)
+export default function addTownRoutes(http: Server, app: Express): void {
+  /*
+   * Create a new user
+   */
+  app.post('/registerUser', BodyParser.json(), async (req, res) => {
+    try {
+      const result = await userRegistrationRequestHandler({
+        name: req.body.name,
+        emailId: req.body.emailId,
+        password: req.body.password,
+      });
+      res.status(StatusCodes.OK)
+        .json(result);
+    } catch (err) {
+      logError(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
         .json({
-          errorMessage: 'Unauthorized Request, please login first to perform the operation',
+          message: 'Internal server error, please see log in server for more details',
         });
     }
-    // otherwise, return a bad request error
-    return res.status(400).json({ errorMessage: 'Bad Request' });
-  }
-};
+  });
 
-export default { routes, validateAPIRequest };
+  /*
+   * Login user
+   */
+  app.post('/loginUser', BodyParser.json(), async (req, res) => {
+    try {
+      const result = await userLoginRequestHandler({
+        emailId: req.body.emailId,
+        password: req.body.password,
+      });
+      res.status(StatusCodes.OK)
+        .json(result);
+    } catch (err) {
+      logError(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          message: 'Internal server error, please see log in server for more details',
+        });
+    }
+  });
+
+  /*
+   * Login user
+   */
+  app.post('/logoutUser', BodyParser.json(), async (req, res) => {
+    try {
+      const result = await userLogoutRequestHandler(req.cookies.userSession);
+      res.status(StatusCodes.OK)
+        .json(result);
+    } catch (err) {
+      logError(err);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          message: 'Internal server error, please see log in server for more details',
+        });
+    }
+  });
+
+}
+
+
+// export default { routes, validateAPIRequest };
--- services/roomService/src/types/user.ts
+++ services/roomService/src/types/user.ts
@@ -1,11 +1,33 @@
-import { Document, Model } from 'mongoose';
+import { nanoid } from 'nanoid';
 
-export interface IUser {
-  name: string;
-  email: Date;
-  password: string;
-  creationDate?: Date;
+export default class User {
+
+  /** The unique identifier for this player * */
+  private readonly _emailId: string;
+
+  /** The player's name, which is not guaranteed to be unique within the town * */
+  private readonly _name: string;
+
+  /** The player's password set at creation* */
+  private readonly _password: string;
+
+  constructor(name: string, emailId: string, password: string) {
+    this._name = name;
+    this._emailId = emailId;
+    this._password = password;
+
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get emailId(): string {
+    return this._emailId;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
 }
-
-export interface IUserDocument extends IUser, Document {}
-export type IUserModel = Model<IUserDocument>;
