0e2b59b159aa623271a3d77cf4f4b95224620deb
ramtarun <ramtarun.b2013@gmail.com>
2021-04-05 14:44:37 -0400

    format changes

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,16 +1,15 @@
+import { Box, HStack } from '@chakra-ui/react';
 import React, { useState } from 'react';
-import { HStack, Box } from "@chakra-ui/react";
-import PreJoinScreens from '../VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens';
-import MediaErrorSnackbar
-  from '../VideoCall/VideoFrontend/components/PreJoinScreens/MediaErrorSnackbar/MediaErrorSnackbar';
 import { TownJoinResponse } from '../../classes/TownsServiceClient';
 import HistoryMeeting from '../Home/HistoryMeeting';
 import MenuBar from '../Home/MenuBar';
+import MediaErrorSnackbar from '../VideoCall/VideoFrontend/components/PreJoinScreens/MediaErrorSnackbar/MediaErrorSnackbar';
+import PreJoinScreens from '../VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens';
 
 interface LoginProps {
-  doLogin: (initData: TownJoinResponse) => Promise<boolean>
-  logoutHandler: (userName: string) => boolean
-  emailId: string
+  doLogin: (initData: TownJoinResponse) => Promise<boolean>;
+  logoutHandler: (userName: string) => boolean;
+  emailId: string;
 }
 
 export default function Login({ doLogin, logoutHandler, emailId }: LoginProps): JSX.Element {
@@ -21,15 +20,11 @@ export default function Login({ doLogin, logoutHandler, emailId }: LoginProps): 
       <MediaErrorSnackbar error={mediaError} dismissError={() => setMediaError(undefined)} />
       <HStack spacing={8}>
         <Box>
-          <MenuBar logoutHandler={logoutHandler}
-            emailID={emailId} />
+          <MenuBar logoutHandler={logoutHandler} emailID={emailId} />
           <HistoryMeeting />
         </Box>
         <Box>
-          <PreJoinScreens
-            doLogin={doLogin}
-            setMediaError={setMediaError}
-          />
+          <PreJoinScreens doLogin={doLogin} setMediaError={setMediaError} />
         </Box>
       </HStack>
     </>
