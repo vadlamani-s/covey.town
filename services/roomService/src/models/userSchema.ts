ef3669bcb73117026f816f5a470861a3db9c3b93
Satya <vadlamani.s@husky.neu.edu>
2021-04-12 15:26:46 -0400

    Changes to tsconfig.json

--- services/roomService/src/models/userSchema.ts
+++ services/roomService/src/models/userSchema.ts
@@ -1,8 +1,7 @@
 /* eslint-disable */
 import bcrypt from 'bcryptjs';
-import { mongo, Mongoose, Schema, model } from 'mongoose';
-import { userInfo } from 'os';
-import { IUser, IUserDocument } from '../types/IUser';
+import { model, Schema } from 'mongoose';
+import { IUserDocument } from '../types/IUser';
 
 const SALT_WORK_FACTOR = 10;
 
@@ -12,28 +11,6 @@ const LoginSchema = new Schema<IUserDocument>({
   password: { type: String, required: true },
   creationDate: { type: Date, default: new Date() },
 });
- 
-
-// LoginSchema.pre('save', function (next) {
-//   const user = this;
-
-//   // only hash the password if it has been modified (or is new)
-//   if (!user.isModified('password')) return next();
-
-//   // generate a salt
-//   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-//     if (err) return next(err);
-
-//     // hash the password using our new salt
-//     bcrypt.hash(user.password, salt, function (err, hash) {
-//       if (err) return next(err);
-
-//       // override the cleartext password with the hashed one
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
 
 export function generateHash(password: string): string {
   const salt = bcrypt.genSaltSync();
@@ -41,14 +18,4 @@ export function generateHash(password: string): string {
   return hashedPassword;
 }
 
-
-// LoginSchema.methods.comparePassword = async function(candidatePassword) {
-//   try {
-//     const isMatch = await bcrypt.compare(candidatePassword, this.password);
-//     return isMatch;
-//   } catch (err) {
-//     throw Error(err);
-//   }
-// };
-
-export const UserModel = model('user', LoginSchema, "users");
+export const UserModel = model('user', LoginSchema, 'users');
